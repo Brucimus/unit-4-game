e2c9fdb9734cae95b959d9703a867e5a0ee9dea2
Brucimus <Brucimus@gmail.com>
2018-06-16 23:25:30 -0700

    added function that checks wins, losses, current value

--- javascript/game.js
+++ javascript/game.js
@@ -17,56 +17,71 @@ gemValue.heart = Math.floor(Math.random() * 12) + 1;
 gemValue.princess = Math.floor(Math.random() * 12) +1;
 gemValue.triangle = Math.floor(Math.random() * 12) +1;
 
+// display target Value on html
+var htmlTargetValue = 
+    "<p>Target Value: " + targetNum + "</p>";
+
+document.querySelector("#targetValue").innerHTML = htmlTargetValue;
+
+
+
+function refreshValCheckEnd() {
+// //display target number on html
+    var htmlCurrentValue = 
+        "<p>Current Value: " + userGuessValue + "</p>";
+
+    document.querySelector("#currentValue").innerHTML = htmlCurrentValue;
+
+    if (userGuessValue === targetNum) {
+        wins++;
+        alert("You win!");
+    } else if (userGuessValue > targetNum) {
+        //if over then lose
+        losses++;
+        alert("You lose!");
+    }
+
+    //display wins on html
+    var htmlWins = 
+        "<p>Wins: " + wins + "</p>";
+
+    document.querySelector("#wins").innerHTML = htmlWins;
+
+    // //display losses on html
+    var htmlLosses = 
+        "<p>Losses: " + losses + "</p>";
+
+    document.querySelector("#losses").innerHTML = htmlLosses;
+}
+
+refreshValCheckEnd();
+
 //add value to accumulated gems every click
-//$(".crystal-image").on("click", function()
 $("#cushion").on("click", function() {
     userGuessValue += gemValue.cushion;
-    //alert(userGuessValue);
+    // alert(userGuessValue);
+    refreshValCheckEnd();
 })
 
 $("#heart").on("click", function() {
     userGuessValue += gemValue.heart;
-    //alert(userGuessValue);
+    // alert(userGuessValue);
+    refreshValCheckEnd();
 })
 
 $("#princess").on("click", function() {
     userGuessValue += gemValue.princess;
-    //alert(userGuessValue);
+    // alert(userGuessValue);
+    var htmlCurrentValue = 
+    "<p>Current Value: " + userGuessValue + "</p>";
+    refreshValCheckEnd();
 })
 
 $("#triangle").on("click", function() {
     userGuessValue += gemValue.triangle;
-    //alert(userGuessValue);
+    // alert(userGuessValue);
+    refreshValCheckEnd();
 })
 
 //compare the accumulated value with the goal value
 
-//if equal then win
-if (userGuessValue === targetNum) {
-    win++;
-    alert("You win!");
-} else if (userGuessValue > targetNum) {
-    //if over then lose
-    losses++;
-    alert("You lose!");
-}
-
-//display wins on html
-var htmlWins = 
-    "<p>Wins: " + wins + "</p>";
-
-document.querySelector("#wins").innerHTML = htmlWins;
-
-//display losses on html
-var htmlLosses = 
-    "<p>Losses: " + losses + "</p>";
-
-document.querySelector("#losses").innerHTML = htmlLosses;
-
-//display target number on html
-var htmlCurrentValue = 
-    "<p>Current Value: " + userGuessValue + "</p>";
-
-document.querySelector("#currentValue").innerHTML = htmlCurrentValue;
-
-//display user guess value
